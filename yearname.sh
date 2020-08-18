912b94bd6eebd81ffc3ccc9a14234ff9f96868a9
unknown <email_address>
2020-08-18 19:44:44 +0530

    added yearname file

---
+++ yearname.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+read -p "Enter the day " d
+read -p " Enter the month" m
+read -p "Enter the year" y
+
+Yo=$(( y-(14-m)-12 ))
+b=$(( Yo+Yo/4-Yo/100+Yo/400 ))
+c=$(( m+12*((14-m)/12)-2))
+e=$(( (d+b+31*c/12)%7 ))
+echo "day of the week is $e"
+
