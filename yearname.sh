1d0632a46e2fec0a2ac8c0c7ff11cf56b19f0bb9
unknown <email_address>
2020-08-18 21:43:15 +0530

    edited the code of yearname.sh

--- yearname.sh
+++ yearname.sh
@@ -1,11 +1,43 @@
 #! /bin/bash
-read -p "Enter the day " d
-read -p " Enter the month" m
-read -p "Enter the year" y
 
-Yo=$(( y-(14-m)-12 ))
-b=$(( Yo+Yo/4-Yo/100+Yo/400 ))
-c=$(( m+12*((14-m)/12)-2))
-e=$(( (d+b+31*c/12)%7 ))
-echo "day of the week is $e"
-echo "thank you"
+read -p "Enter the day : " d
+read -p " Enter the month : " m
+read -p "Enter the year : " y
+
+Yo=$(( y-((14-m)/12) ))
+X=$(( Yo+(Yo/4)-(Yo/100)+(Yo/400) ))
+Mo=$(( m+12*((14-m)/12)-2 ))
+Do=$(( (d+X+31*Mo/12)%7 ))
+
+
+   if [ $Do == 0 ]   
+      then
+      echo "Today is sunday"
+    elif [ $Do == 1 ]
+      then
+      echo "Today is Monday"
+    elif [ $Do == 2 ]
+      then
+      echo "Today is Tuesday"
+    elif [ $Do == 3 ]
+      then
+      echo "Today is Wednesday"
+    elif [ $Do == 4 ]
+      then
+      echo "Today is Thurday"
+    elif [ $Do == 5 ]
+      then
+      echo "Today is friday"
+    elif [ $Do == 6 ]
+      then
+      echo "Today is Saturday"
+    else
+      echo "Please enter the correct format of date-month-year"
+
+   fi
+
+
+echo "$Yo" 
+echo "$X"
+echo "$Mo"
+echo "$Do"
